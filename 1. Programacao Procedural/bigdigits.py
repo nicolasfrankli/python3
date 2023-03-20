58ae438c6069a1f971716a7221f51f81f13d40ba
Nicolas <nicolasgabrielolifranklin@gmail.com>
2023-03-20 10:39:07 -0300

    Adicionado mecanismo para argumentos nulos

--- 1. Programacao Procedural/bigdigits.py
+++ 1. Programacao Procedural/bigdigits.py
@@ -21,23 +21,27 @@ Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
 bigdigits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
 def main(): 
-    while True:
-        try :
-            number = argv[1]
-            break
-        except ValueError as err:
-            print(err)
-            continue
-        except IndexError:
-            print("usage: bigdigits.py <number>")
-        except EOFError:
-            break
+    if len(argv) == 2: #Verifica se o número foi fornecido na linha de comando
+        while True:             
+            try :
+                number = argv[1]
+                break
+            except None:
+                continue
+            except ValueError as err:
+                print(err)
+                continue
+            except IndexError:
+                print("usage: bigdigits.py <number>")
+            except EOFError:
+                break
 
-    str_number = str(number)
-    for c in range(7):
-        output = ''
-        for count in range(len(str_number)):
-            output += bigdigits[int(str_number[count])][c]  +  '   '
-        print(output)
-
+        str_number = str(number)
+        for c in range(7): #Cada algarismo da lista de grandes dígitos tem 7 itens ou 7 linhas
+            output = ''
+            for count in range(len(str_number)):
+                output += bigdigits[int(str_number[count])][c]  +  '   ' #Cria uma string com as linhas em ordem de cada grande dígito
+            print(output)
+    else:
+        print("No command line arguments")
 main()
